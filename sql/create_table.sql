fe6377a41453c429e52d43106664e4bbb7be1af5
chengfushi <303623518@qq.com>
2025-08-29 22:41:09 +0800

    refactor(sql): 移除用户表中的冗余索引
    - 删除了用户表中的 uk_userAccount 唯一索引
    - 保留了 idx_userName索引

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -14,6 +14,6 @@ create table if not exists user
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
-    UNIQUE KEY uk_userAccount (userAccount),
     INDEX idx_userName (userName)
     ) comment '用户' collate = utf8mb4_unicode_ci;
+
